90e02b9e9a608056b3120a2130f38582a94ca2cd
Tella Adesina <telladesina100@gmail.com>
2021-07-08 12:41:55 -0500

    Commit08972021VG

---
+++ MISWireframe/BackofficeWireframV2/Controllers/DiginetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackofficeWireframV2.Controllers
+{
+    public class DiginetController : Controller
+    {
+        // GET: Diginet
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult TradeAction()
+        {
+            return View();
+        }
+        public ActionResult GoldLedger()
+        {
+            return View();
+        }
+        public ActionResult SilverLedger()
+        {
+            return View();
+        }
+        public ActionResult DigiTokenLedger()
+        {
+            return View();
+        }
+    }
+}
