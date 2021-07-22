9cfcad1afaba051a52d429cd8f4fd384fd6d0bce
Tella Adesina <telladesina100@gmail.com>
2021-07-22 18:47:34 -0500

    Commit22072021-Kill

--- MISWireframe/BackofficeWireframV2/Controllers/DiginetController.cs
+++ MISWireframe/BackofficeWireframV2/Controllers/DiginetController.cs
@@ -17,16 +17,16 @@ namespace BackofficeWireframV2.Controllers
         [HttpGet]
         public ActionResult Login(string returnUrl, string username, string password)
         {
-            return new FilePathResult("~/diginet-html/login.html", "text/html");
+            return  View("~/diginet-html/login.html");
         }
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
             if(username == null && password == null)
-            return new FilePathResult("~/diginet-html/login.html", "text/html");
+            return View("~/diginet-html/login.html");
             else
             {
-                return new FilePathResult("~/diginet-html/index.html", "text/html");
+                return View("~/diginet-html/index.html");
             }
         }
 
