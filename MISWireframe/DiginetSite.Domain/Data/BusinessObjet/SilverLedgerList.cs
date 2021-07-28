19300b224318373fffaf1d9b65a66180f17696ba
Tella Adesina <telladesina100@gmail.com>
2021-07-28 17:56:37 -0500

    Commit28072021-jk

---
+++ MISWireframe/DiginetSite.Domain/Data/BusinessObjet/SilverLedgerList.cs
@@ -0,0 +1,13 @@
+﻿using DiginetSite.Domain.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiginetSite.Domain.Data.BusinessObjet
+{
+    public class SilverLedgerList : List<SilverLedgerViewModel>
+    {
+    }
+}
---
+++ MISWireframe/DiginetSite.Domain/Data/DAL/DiginetDb.cs
@@ -0,0 +1,50 @@
+﻿using DiginetSite.Domain.Data.BusinessObjet;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiginetSite.Domain.Data.DAL
+{
+    public class DiginetDb
+    {
+        public GoldLedgerList GetGoldLedger()
+        {
+            GoldLedgerList list = new GoldLedgerList();
+
+            string com = ConfigurationManager.ConnectionStrings["DiginetDB"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(com))
+            {
+                
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Retrieve_Record_Proc", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Name", "HardDisk"));
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+
+                    }
+                    return null;
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+            }
+
+                var query = $"Select ";
+        }
+    }
+}
